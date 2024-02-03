9b56c1e6c2ff92053cb67f28757482e542e13c29
dwielok <dwieloknuraini773@gmail.com>
2024-02-03 21:00:36 +0700

    group menu seeder

--- database/seeders/SGroupSeeder.php
+++ database/seeders/SGroupSeeder.php
@@ -15,7 +15,7 @@ class SGroupSeeder extends Seeder
     public function run()
     {
         DB::table('s_group')->insert([
-            ['group_id' => 1, 'group_code' => 'SPR', 'group_name' => 'Super Admin'],
+            ['group_id' => 1, 'group_code' => 'KOM', 'group_name' => 'Koordinator MBKM'],
             ['group_id' => 2, 'group_code' => 'ADM', 'group_name' => 'Admin'],
             ['group_id' => 3, 'group_code' => 'DSN', 'group_name' => 'Dosen'],
             ['group_id' => 4, 'group_code' => 'MHS', 'group_name' => 'Mahasiswa'],
