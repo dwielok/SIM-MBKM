ce8e257990b17e444d1102a08b212d2799f0c53e
dwielok <dwieloknuraini773@gmail.com>
2024-02-03 20:58:02 +0700

    migration menu

--- database/seeders/SMenuSeeder.php
+++ database/seeders/SMenuSeeder.php
@@ -16,13 +16,22 @@ class SMenuSeeder extends Seeder
     {
         // for Super Admin and Admin
         DB::table('s_menu')->upsert([
-            ['menu_id' => '1','menu_scope' => 'ALL','menu_code' => 'DASHBOARD','menu_name' => 'Dashboard','menu_url' => '/','menu_level' => '1','order_no' => '1','parent_id' => NULL,'class_tag' => 'dashboard','icon' => 'fas fa-tachometer-alt','is_active' => '1'],
-            ['menu_id' => '2','menu_scope' => 'ADMIN','menu_code' => 'MASTER','menu_name' => 'Data Master','menu_url' => NULL,'menu_level' => '1','order_no' => '2','parent_id' => NULL,'class_tag' => 'master','icon' => 'fas fa-th','is_active' => '1'],
-            ['menu_id' => '3','menu_scope' => 'ADMIN','menu_code' => 'TRANSACTION','menu_name' => 'Transaksi','menu_url' => NULL,'menu_level' => '1','order_no' => '3','parent_id' => NULL,'class_tag' => 'transaction','icon' => 'fas fa-edit','is_active' => '1'],
-            ['menu_id' => '4','menu_scope' => 'ADMIN','menu_code' => 'REPORT','menu_name' => 'Laporan','menu_url' => NULL,'menu_level' => '1','order_no' => '4','parent_id' => NULL,'class_tag' => 'report','icon' => 'fas fa-file-invoice','is_active' => '1'],
-            ['menu_id' => '5','menu_scope' => 'SUPER','menu_code' => 'SETTING','menu_name' => 'Setting','menu_url' => NULL,'menu_level' => '1','order_no' => '5','parent_id' => NULL,'class_tag' => 'setting','icon' => 'fas fa-cogs','is_active' => '1'],
-            ['menu_id' => '6','menu_scope' => 'ADMIN','menu_code' => 'MASTER.JURUSAN','menu_name' => 'Jurusan','menu_url' => 'master/jurusan','menu_level' => '2','order_no' => '22','parent_id' => '2','class_tag' => 'master-jurusan','icon' => 'fas fa-minus text-xs','is_active' => '1'],
-            ['menu_id' => '7','menu_scope' => 'ADMIN','menu_code' => 'MASTER.PRODI','menu_name' => 'Program Studi','menu_url' => 'master/prodi','menu_level' => '2','order_no' => '23','parent_id' => '2','class_tag' => 'master-prodi','icon' => 'fas fa-minus text-xs','is_active' => '1'],
+            ['menu_id' => '1', 'menu_scope' => 'ALL', 'menu_code' => 'DASHBOARD', 'menu_name' => 'Dashboard', 'menu_url' => '/', 'menu_level' => '1', 'order_no' => '1', 'parent_id' => NULL, 'class_tag' => 'dashboard', 'icon' => 'fas fa-tachometer-alt', 'is_active' => '1'],
+            ['menu_id' => '2', 'menu_scope' => 'ADMIN', 'menu_code' => 'MASTER', 'menu_name' => 'Data Master', 'menu_url' => NULL, 'menu_level' => '1', 'order_no' => '3', 'parent_id' => NULL, 'class_tag' => 'master', 'icon' => 'fas fa-th', 'is_active' => '1'],
+            ['menu_id' => '3', 'menu_scope' => 'ADMIN', 'menu_code' => 'TRANSACTION', 'menu_name' => 'Transaksi', 'menu_url' => NULL, 'menu_level' => '1', 'order_no' => '4', 'parent_id' => NULL, 'class_tag' => 'transaction', 'icon' => 'fas fa-edit', 'is_active' => '1'],
+            ['menu_id' => '4', 'menu_scope' => 'ADMIN', 'menu_code' => 'REPORT', 'menu_name' => 'Laporan', 'menu_url' => NULL, 'menu_level' => '1', 'order_no' => '5', 'parent_id' => NULL, 'class_tag' => 'report', 'icon' => 'fas fa-file-invoice', 'is_active' => '1'],
+            ['menu_id' => '5', 'menu_scope' => 'SUPER', 'menu_code' => 'SETTING', 'menu_name' => 'Setting', 'menu_url' => NULL, 'menu_level' => '1', 'order_no' => '6', 'parent_id' => NULL, 'class_tag' => 'setting', 'icon' => 'fas fa-cogs', 'is_active' => '1'],
+            ['menu_id' => '6', 'menu_scope' => 'ADMIN', 'menu_code' => 'MASTER.JURUSAN', 'menu_name' => 'Jurusan', 'menu_url' => 'master/jurusan', 'menu_level' => '2', 'order_no' => '1', 'parent_id' => '2', 'class_tag' => 'master-jurusan', 'icon' => 'fas fa-minus text-xs', 'is_active' => '1'],
+            ['menu_id' => '7', 'menu_scope' => 'ADMIN', 'menu_code' => 'MASTER.PRODI', 'menu_name' => 'Program Studi', 'menu_url' => 'master/prodi', 'menu_level' => '2', 'order_no' => '2', 'parent_id' => '2', 'class_tag' => 'master-prodi', 'icon' => 'fas fa-minus text-xs', 'is_active' => '1'],
+            ['menu_id' => '8', 'menu_scope' => 'ADMIN', 'menu_code' => 'MASTER.PROGRAM', 'menu_name' => 'Program', 'menu_url' => 'master/program', 'menu_level' => '2', 'order_no' => '3', 'parent_id' => '2', 'class_tag' => 'master-program', 'icon' => 'fas fa-minus text-xs', 'is_active' => '1'],
+            ['menu_id' => '9', 'menu_scope' => 'ADMIN', 'menu_code' => 'MASTER.KEGIATAN', 'menu_name' => 'Kegiatan', 'menu_url' => 'master/kegiatan', 'menu_level' => '2', 'order_no' => '4', 'parent_id' => '2', 'class_tag' => 'master-kegiatan', 'icon' => 'fas fa-minus text-xs', 'is_active' => '1'],
+            ['menu_id' => '10', 'menu_scope' => 'ADMIN', 'menu_code' => 'MASTER.PERIODE', 'menu_name' => 'Periode', 'menu_url' => 'master/periode', 'menu_level' => '2', 'order_no' => '5', 'parent_id' => '2', 'class_tag' => 'master-periode', 'icon' => 'fas fa-minus text-xs', 'is_active' => '1'],
+            ['menu_id' => '11', 'menu_scope' => 'ADMIN', 'menu_code' => 'MASTER.MAHASISWA', 'menu_name' => 'Mahasiswa', 'menu_url' => 'master/mahasiswa', 'menu_level' => '2', 'order_no' => '6', 'parent_id' => '2', 'class_tag' => 'master-mahasiswa', 'icon' => 'fas fa-minus text-xs', 'is_active' => '1'],
+            ['menu_id' => '12', 'menu_scope' => 'ADMIN', 'menu_code' => 'TRANSACTION.PENDAFTARAN', 'menu_name' => 'Pendaftaran', 'menu_url' => 'transaksi/pendaftaran', 'menu_level' => '2', 'order_no' => '1', 'parent_id' => '3', 'class_tag' => 'transaksi-pendaftaran', 'icon' => 'fas fa-minus text-xs', 'is_active' => '1'],
+            ['menu_id' => '13', 'menu_scope' => 'ALL', 'menu_code' => 'MITRA', 'menu_name' => 'Mitra', 'menu_url' => 'mitra', 'menu_level' => '1', 'order_no' => '2', 'parent_id' => '2', 'class_tag' => 'mitra', 'icon' => 'fas fa-bulding text-xs', 'is_active' => '1'],
+
+
+
         ], ['menu_id', 'menu_code'], ['menu_scope', 'menu_name', 'menu_url', 'class_tag']);
     }
 }
