38a4035eef26cc53f0c007a4addb10f916cb30ce
dwielok <dwieloknuraini773@gmail.com>
2024-03-04 14:59:02 +0700

    feat: berita

--- app/Http/Controllers/Transaction/BeritaController.php
+++ app/Http/Controllers/Transaction/BeritaController.php
@@ -3,9 +3,231 @@
 namespace App\Http\Controllers\Transaction;
 
 use App\Http\Controllers\Controller;
+use App\Models\Master\ProdiModel;
+use App\Models\Transaction\BeritaModel;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Str;
 
 class BeritaController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->menuCode  = 'TRANSACTION.BERITA';
+        $this->menuUrl   = url('transaksi/berita');     // set URL untuk menu ini
+        $this->menuTitle = 'Berita';                       // set nama menu
+        $this->viewPath  = 'transaction.berita.';         // untuk menunjukkan direktori view. Diakhiri dengan tanda titik
+    }
+
+    public function index()
+    {
+        $this->authAction('read');
+        $this->authCheckDetailAccess();
+
+        $breadcrumb = [
+            'title' => $this->menuTitle,
+            'list'  => ['Transaksi', 'Berita']
+        ];
+
+        $activeMenu = [
+            'l1' => 'transaction',
+            'l2' => 'transaksi-berita',
+            'l3' => null
+        ];
+
+        $page = [
+            'url' => $this->menuUrl,
+            'title' => 'Daftar ' . $this->menuTitle
+        ];
+
+        $prodis = ProdiModel::select('prodi_id', 'prodi_name', 'prodi_code')->get();
+
+        return view($this->viewPath . 'index')
+            ->with('breadcrumb', (object) $breadcrumb)
+            ->with('activeMenu', (object) $activeMenu)
+            ->with('page', (object) $page)
+            ->with('prodis', $prodis)
+            ->with('allowAccess', $this->authAccessKey());
+    }
+
+    public function list(Request $request)
+    {
+        $this->authAction('read', 'json');
+        if ($this->authCheckDetailAccess() !== true) return $this->authCheckDetailAccess();
+
+        $data  = BeritaModel::all();
+        $data = $data->map(function ($item) {
+            $item->tanggal = Carbon::parse($item->created_at)->format('d/m/Y');
+            $item->prodi = $item->prodi ? ProdiModel::where('prodi_id', $item->prodi_id)->first() : NULL;
+            return $item;
+        });
+
+        return DataTables::of($data)
+            ->addIndexColumn()
+            ->make(true);
+    }
+
+    public function create()
+    {
+        $this->authAction('create', 'modal');
+        if ($this->authCheckDetailAccess() !== true) return $this->authCheckDetailAccess();
+
+        $page = [
+            'url' => $this->menuUrl,
+            'title' => 'Tambah ' . $this->menuTitle
+        ];
+
+        $prodis = ProdiModel::select('prodi_id', 'prodi_name', 'prodi_code')->get();
+
+        return view($this->viewPath . 'action')
+            ->with('page', (object) $page)
+            ->with('prodis', $prodis);
+    }
+
+    public function store(Request $request)
+    {
+        $this->authAction('create', 'json');
+        if ($this->authCheckDetailAccess() !== true) return $this->authCheckDetailAccess();
+
+        if ($request->ajax() || $request->wantsJson()) {
+
+            $rules = [
+                'berita_judul' => 'required',
+                'berita_isi' => 'required',
+                'berita_status' => 'required|integer',
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'stat'     => false,
+                    'mc'       => false,
+                    'msg'      => 'Terjadi kesalahan.',
+                    'msgField' => $validator->errors()
+                ]);
+            }
+
+            $request['berita_uid'] = Str::uuid()->toString();
+            $res = BeritaModel::insertData($request);
+
+            return response()->json([
+                'stat' => $res,
+                'mc' => $res, // close modal
+                'msg' => ($res) ? $this->getMessage('insert.success') : $this->getMessage('insert.failed')
+            ]);
+        }
+
+        return redirect('/');
+    }
+
+    public function edit($id)
+    {
+        $this->authAction('update', 'modal');
+        if ($this->authCheckDetailAccess() !== true) return $this->authCheckDetailAccess();
+
+        $page = [
+            'url' => $this->menuUrl . '/' . $id,
+            'title' => 'Edit ' . $this->menuTitle
+        ];
+
+        $data = BeritaModel::find($id);
+        $prodis = ProdiModel::select('prodi_id', 'prodi_name', 'prodi_code')->get();
+
+        return (!$data) ? $this->showModalError() :
+            view($this->viewPath . 'action')
+            ->with('page', (object) $page)
+            ->with('id', $id)
+            ->with('prodis', $prodis)
+            ->with('data', $data);
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $this->authAction('update', 'json');
+        if ($this->authCheckDetailAccess() !== true) return $this->authCheckDetailAccess();
+
+        if ($request->ajax() || $request->wantsJson()) {
+
+            $rules = [
+                'berita_judul' => 'required',
+                'berita_isi' => 'required',
+                'berita_status' => 'required|integer',
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'stat'     => false,
+                    'mc'       => false,
+                    'msg'      => 'Terjadi kesalahan.',
+                    'msgField' => $validator->errors()
+                ]);
+            }
+
+            $res = BeritaModel::updateData($id, $request);
+
+            return response()->json([
+                'stat' => $res,
+                'mc' => $res, // close modal
+                'msg' => ($res) ? $this->getMessage('update.success') : $this->getMessage('update.failed')
+            ]);
+        }
+
+        return redirect('/');
+    }
+
+    public function show($id)
+    {
+        $this->authAction('read', 'modal');
+        if ($this->authCheckDetailAccess() !== true) return $this->authCheckDetailAccess();
+
+        $data = BeritaModel::find($id);
+        $page = [
+            'title' => 'Detail ' . $this->menuTitle
+        ];
+
+        return (!$data) ? $this->showModalError() :
+            view($this->viewPath . 'detail')
+            ->with('page', (object) $page)
+            ->with('id', $id)
+            ->with('data', $data);
+    }
+
+    public function confirm($id)
+    {
+        $this->authAction('delete', 'modal');
+        if ($this->authCheckDetailAccess() !== true) return $this->authCheckDetailAccess();
+
+        $data = BeritaModel::find($id);
+
+        return (!$data) ? $this->showModalError() :
+            $this->showModalConfirm($this->menuUrl . '/' . $id, [
+                'Topik' => $data->berita_judul,
+                // 'Deskripsi' => $data->berita_isi
+            ]);
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        $this->authAction('delete', 'json');
+        if ($this->authCheckDetailAccess() !== true) return $this->authCheckDetailAccess();
+
+        if ($request->ajax() || $request->wantsJson()) {
+
+            $res = BeritaModel::deleteData($id);
+
+            return response()->json([
+                'stat' => $res,
+                'mc' => $res, // close modal
+                'msg' => BeritaModel::getDeleteMessage()
+            ]);
+        }
+
+        return redirect('/');
+    }
 }
